8e82b2cae5bfd8a09505b7b7d9232856da90a249
Aditya Ladwa <ladwa.aditya@gmail.com>
2018-08-15 17:17:46 +0530

    Added MSQL create and read DB

--- ch05/02-create-record-mysql.go
+++ ch05/02-create-record-mysql.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -39,10 +42,33 @@ func getCurrentDb(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Current Database is :: %s", db)
 }
 
+func createRecord(w http.ResponseWriter, r *http.Request) {
+	vals := r.URL.Query()
+	name, ok := vals["name"]
+	if ok {
+		log.Print("Going to insert record in database for name:", name[0])
+		stmt, err := db.Prepare("INSERT employee SET name=?")
+		if err != nil {
+			log.Print("error preparing query :: ", err)
+			return
+		}
+		result, err := stmt.Exec(name[0])
+		if err != nil {
+			log.Print("error executing query :: ", err)
+			return
+		}
+		id, err := result.LastInsertId()
+		fmt.Fprintf(w, "Last Inserted Record Id is :: %s", strconv.FormatInt(id, 10))
+	} else {
+		fmt.Fprintf(w, "Error occurred while creating record in database for name :: %s", name[0])
+	}
+}
+
 func main() {
-	http.HandleFunc("/", getCurrentDb)
+	router := mux.NewRouter()
+	router.HandleFunc("/employee/create", createRecord).Methods("POST")
 	defer db.Close()
-	err := http.ListenAndServe(HOST+":"+PORT, nil)
+	err := http.ListenAndServe(HOST+":"+PORT, router)
 	if err != nil {
 		log.Fatal("error starting http server :: ", err)
 		return
